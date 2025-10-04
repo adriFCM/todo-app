8aa04e1bd9498f35b2c84fc6d05eaf119e75180c
adriFCM <acastanon766@gmail.com>
2025-10-04 17:29:02 +0200

    chore(urls): clarify routing with brief comments

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView   
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), # not used in this project, kept for possible future inprovements
     path('tasks/', include('tasks.urls')),     
     path('', RedirectView.as_view(               
         pattern_name='task_list',                
